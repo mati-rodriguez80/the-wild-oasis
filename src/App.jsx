d758b7fe341964fee8de5635196f529778159f8d
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-06-29 19:55:44 -0300

    Create GlobalStyles.js file which will hold all the global styles of the app and include it in App component on top and as a sibling of the other elements, delete index.css file, add the Google fonts in index.html file, and start doing some tests using styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import styled from "styled-components";
+
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.main`
+  background-color: var(--color-brand-100);
+  padding: 20px;
+`;
+
 function App() {
-  return <h1>The Wild Oasis</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check in")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
