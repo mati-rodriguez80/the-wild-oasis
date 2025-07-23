bca1b1ce142387839a45762ad66577f0e55ba45c
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-07-22 22:07:31 -0300

    Convert the Modal to a Compound Component: Create the ModalContext and provide it in the Modal parent component which has the state and handler functions, create the Open and Window child components that use cloneElement to add props to the whatever children it receives, and use this new compound component in AddCabin component to set the cabin-form modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 
@@ -52,34 +53,63 @@ const Button = styled.button`
   }
 `;
 
-// A React Portal is a feature that essentially allow us to render an element "outside of the parent's component
-// DOM Structure" while still keeping the elements in "the original position of the Component Tree". In other words,
-// with a Portal we can basically render a component in any place that we want inside the DOM tree but still
-// leave the component at the same place in the React component tree, and so then things like props keep working
-// normally. And so, this is great and generally used for all elements that we want to stay on top of other elements
-// like modals windows, tooltips, menus, and so on. We implement this by using createPortal which needs the JSX first,
-// and then the parent DOM element.
+const ModalContext = createContext();
 
-function Modal({ children, onClose }) {
+function Modal({ children }) {
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
+
+  return (
+    <ModalContext.Provider value={{ openName, close, open }}>{children}</ModalContext.Provider>
+  );
+}
+
+function Open({ children, opens: opensWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  // cloneElement lets you create a new React element using another element as a starting point
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+}
+
+function Window({ children, name }) {
+  const { openName, close } = useContext(ModalContext);
+
+  // This guard clause is for when we have two Windows within a unique Modal component, so then one of the two windows
+  // will then have the name equal to cabin-form or table, for instance.
+  if (name !== openName) return null;
+
+  // A React Portal is a feature that essentially allow us to render an element "outside of the parent's component
+  // DOM Structure" while still keeping the elements in "the original position of the Component Tree". In other words,
+  // with a Portal we can basically render a component in any place that we want inside the DOM tree but still
+  // leave the component at the same place in the React component tree, and so then things like props keep working
+  // normally. And so, this is great and generally used for all elements that we want to stay on top of other elements
+  // like modals windows, tooltips, menus, and so on. We implement this by using createPortal which needs the JSX first,
+  // and then the parent DOM element.
+
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={onClose}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
 
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
+
+  // Now, we might be wondering, this already works great without the Portal and with just regular CSS positioning,
+  // so why we even need to use this Portal? The main reason why a Portal becomes necessary is in order to avoid
+  // conflicts with the CSS property "overflow: hidden". Many times we build a component like a modal and it works
+  // just fine, but then some other developer will re-use it somewhere else, and that might be a place where the modal
+  // will get cut off by an overflow hidden set on the parent. So, this is basically all about reusability, and in
+  // order to avoid these situations will simply render the modal completely outside the rest of the DOM.
 }
 
-// Now, we might be wondering, this already works great without the Portal and with just regular CSS positioning,
-// so why we even need to use this Portal? The main reason why a Portal becomes necessary is in order to avoid
-// conflicts with the CSS property "overflow: hidden". Many times we build a component like a modal and it works
-// just fine, but then some other developer will re-use it somewhere else, and that might be a place where the modal
-// will get cut off by an overflow hidden set on the parent. So, this is basically all about reusability, and in
-// order to avoid these situations will simply render the modal completely outside the rest of the DOM.
+Modal.Open = Open;
+Modal.Window = Window;
 
 export default Modal;
