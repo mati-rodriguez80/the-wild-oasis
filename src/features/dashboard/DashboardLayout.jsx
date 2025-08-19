36317e2bdfb4da3474a3ac51d25cb7f8ac3c3fe8
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-08-18 23:35:31 -0300

    Start building the dashboard: Add DashboardFilter in Dashboard page to filter for the last 7, 30 or 90 days, and add DashboardLayout which uses two new custom hooks: useRecentBookings which uses getBookingsAfterDate from apiBookings.js where we compute the date using the last query param, and useRecentStays which uses getStaysAfterDate which does the same but also computes the confirmed stays filtering by checked in and checked out stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,31 @@
 import styled from "styled-components";
 
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+
+import Spinner from "../../ui/Spinner";
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const { stays, isLoading: isLoadingStays, confirmedStays } = useRecentStays();
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
