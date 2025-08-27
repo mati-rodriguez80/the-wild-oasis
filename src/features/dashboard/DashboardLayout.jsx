b4f695b942ee2b29db6a6f5865cb5fe350ef108e
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-08-26 21:35:04 -0300

    Display a pie chart in the dashboard with the stay duration summary: Add DurationChart component to DashboardLayout passing in the confirmedStays, build the pie chart using recharts library, and use as data the result of using an object with the different durations where the values are being calculated using the prepareData function

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import { useCabins } from "../cabins/useCabins";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -31,7 +32,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today's activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
