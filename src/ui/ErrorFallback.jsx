e75c94daa800a1470a3857e29860393a693f0de4
mati-rodriguez80 <matiasorodriguez89@gmail.com>
2025-08-27 21:26:43 -0300

    Implement error boundary in the app: Install React Error Boundary, include ErrorBoundary component wrapping App component in main.jsx, add ErrorFallback component which will be rendered when there is an error and will display a message and a button that will redirect the user to the home page, and this is possible thanks to include this component in the FallbackComponent prop of ErrorBoundary and the onReset prop

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+import GlobalStyles from "../styles/GlobalStyles";
+
+import Heading from "./Heading";
+import Button from "./Button";
+
 const StyledErrorFallback = styled.main`
   height: 100vh;
   background-color: var(--color-grey-50);
@@ -29,3 +34,26 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+// These error boundaries really only catch errors while React is rendering. So, bugs that occur on some
+// event handlers or in an effect or some asynchronous code won't be caught by this error boundary. But,
+// for those we many times have some other mechanisms like, for example, those errors that are usually
+// returned from useQuery.
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 🧐</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
